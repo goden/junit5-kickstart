3e08f1776b74d9fed8a777188cafdd6a0362a0a6
Gordon <goden.incredible@gmail.com>
2026-05-11 00:22:56 +0800

    recode refactoring.

--- src/test/java/com/gordon/learning/junt5/extension/ApiDebugInfo.java
+++ src/test/java/com/gordon/learning/junt5/extension/ApiDebugInfo.java
@@ -5,36 +5,4 @@ public class ApiDebugInfo {
     public String method;
     public int statusCode;
     public String responseBody;
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getMethod() {
-        return method;
-    }
-
-    public void setMethod(String method) {
-        this.method = method;
-    }
-
-    public int getStatusCode() {
-        return statusCode;
-    }
-
-    public void setStatusCode(int statusCode) {
-        this.statusCode = statusCode;
-    }
-
-    public String getResponseBody() {
-        return responseBody;
-    }
-
-    public void setResponseBody(String responseBody) {
-        this.responseBody = responseBody;
-    }
 }
